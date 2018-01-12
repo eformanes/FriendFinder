1669072b458e437c78e2fc2b3f82fa0d9ba28590
eformanes <30428023+eformanes@users.noreply.github.com>
2018-01-11 23:44:42 -0800

    HTML routing works

---
+++ server.js
@@ -0,0 +1,42 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3001;
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+//require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Basic route that sends the user first to the home.html page
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/app/public/home.html"));
+// });
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
